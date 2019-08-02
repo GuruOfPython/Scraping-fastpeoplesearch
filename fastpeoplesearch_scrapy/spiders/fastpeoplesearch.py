996812be3d7430283ca420503bb333161f48481b
Dusan Pavlovic <45977260+GuruOfPython@users.noreply.github.com>
2019-08-02 05:11:18 -0700

    Update_1

--- fastpeoplesearch_scrapy/spiders/fastpeoplesearch.py
+++ fastpeoplesearch_scrapy/spiders/fastpeoplesearch.py
@@ -288,11 +288,3 @@ class FastpeoplesearchSpider(scrapy.Spider):
 
         yield request
 
-    def create_result_file(self, result_file_name):
-        import codecs
-        self.result_file = codecs.open(result_file_name, "w", "utf-8")
-        self.result_file.write(u'\ufeff')
-
-    def insert_row(self, result_row):
-        self.result_file.write('"' + '","'.join(result_row) + '"' + "\n")
-        self.result_file.flush()
